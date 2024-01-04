a449aa808bdeaf1d7759ffd51279807a103281fc
origin-coding <1604070358@qq.com>
2024-01-04 16:08:25 +0800

    refactor(i18n): now Vuetify uses its own messages instead of custom messages

--- src/plugins/i18n.ts
+++ src/plugins/i18n.ts
@@ -1,4 +1,7 @@
 import { createI18n } from "vue-i18n";
+
+import { en as enVuetify, zhHans as zhHansVuetify } from "vuetify/locale";
+
 import en from "@/locale/en.json";
 import zhHans from "@/locale/zhHans.json";
 
@@ -6,12 +9,21 @@ const i18n = createI18n({
   legacy: false,
   locale: "zhHans",
   fallbackLocale: "en",
-  // Suppress warnings in production mode.
-  missingWarn: import.meta.env.DEV,
-  fallbackWarn: import.meta.env.DEV,
+
+  // Suppress these warnings in development mode.
+  silentTranslationWarn: true,
+  silentFallbackWarn: true,
+
+  // Messages, including custom global messages and Vuetify's messages.
   messages: {
-    en,
-    zhHans,
+    en: {
+      ...en,
+      $vuetify: enVuetify,
+    },
+    zhHans: {
+      ...zhHans,
+      $vuetify: zhHansVuetify,
+    },
   },
 });
 
