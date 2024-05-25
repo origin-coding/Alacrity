31080a9a5fb84e788766727e75d5987d39856bb7
origin-coding <1604070358@qq.com>
2024-05-25 21:57:56 +0800

    feat: add i18n messages for Alacrity's config

--- locales/zhHans/index.ts
+++ locales/zhHans/index.ts
@@ -3,11 +3,13 @@
 import pluginDescriptions from "./plugin-descriptions.json";
 import pluginNames from "./plugin-names.json";
 import groupNames from "./plugin-group-names.json";
+import alacrityConfig from "./alacrity-config.json";
 
 const messages = {
   "plugin-descriptions": pluginDescriptions,
   "plugin-names": pluginNames,
   "plugin-group-names": groupNames,
+  "alacrity-config": alacrityConfig,
 };
 
 console.log(messages);
