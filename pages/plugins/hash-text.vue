6a7b8f2f8459c45a85c8aa029a440cebf4ca5f42
origin-coding <1604070358@qq.com>
2024-06-18 20:50:11 +0800

    feat: implement plugin hash-text

---
+++ pages/plugins/hash-text.vue
@@ -0,0 +1,105 @@
+<script setup lang="ts">
+import {
+  MD5,
+  SHA3,
+  SHA1,
+  SHA224,
+  SHA256,
+  SHA384,
+  SHA512,
+  RIPEMD160,
+  enc,
+} from "crypto-js";
+import { readText, writeText } from "@tauri-apps/api/clipboard";
+
+const { t } = useI18n();
+
+const algorithms = {
+  MD5,
+  SHA1,
+  SHA224,
+  SHA256,
+  SHA384,
+  SHA512,
+  SHA3,
+  RIPEMD160,
+} as const;
+
+type Algorithms = keyof typeof algorithms;
+const nameOptions = Object.keys(algorithms) as Algorithms[];
+const algorithm = ref<Algorithms>("MD5");
+
+const uppercase = ref(false);
+
+const input = ref("");
+
+const output = computed(() => {
+  const hash = algorithms[algorithm.value](input.value).toString(enc.Hex);
+  return uppercase.value ? hash.toUpperCase() : hash;
+});
+
+async function copy() {
+  await writeText(output.value);
+}
+
+function clear() {
+  input.value = "";
+}
+
+async function paste() {
+  input.value = (await readText()) || "";
+}
+</script>
+
+<template>
+  <t-space direction="vertical" w-full>
+    <plugin-name></plugin-name>
+    <option-layout>
+      <t-form-item :label="t('algorithm')">
+        <t-select v-model="algorithm">
+          <t-option v-for="option in nameOptions" :key="option" :value="option">
+            {{ option }}
+          </t-option>
+        </t-select>
+      </t-form-item>
+      <t-form-item :label="t('uppercase')">
+        <t-checkbox v-model="uppercase"></t-checkbox>
+      </t-form-item>
+    </option-layout>
+
+    <t-divider></t-divider>
+
+    <t-space direction="horizontal" w-full>
+      <input-output-layout
+        :clear="true"
+        @clear="clear"
+        :paste="true"
+        @paste="paste"
+      >
+        <t-textarea
+          v-model="input"
+          :clearable="true"
+          :autosize="{ minRows: 10, maxRows: 20 }"
+        ></t-textarea>
+      </input-output-layout>
+      <input-output-layout :copy="true" @copy="copy">
+        <t-textarea :value="output" :readonly="true"></t-textarea>
+      </input-output-layout>
+    </t-space>
+  </t-space>
+</template>
+
+<style scoped lang="less"></style>
+
+<i18n>
+{
+  "en": {
+    "algorithm": "Algorithm",
+    "uppercase": "Uppercase"
+  },
+  "zhHans": {
+    "algorithm": "哈希算法",
+    "uppercase": "是否大写"
+  }
+}
+</i18n>
---
+++ plugins/003.hash-text.ts
@@ -0,0 +1,18 @@
+// noinspection JSUnusedGlobalSymbols
+
+import { AlacrityPluginType } from "~/types/alacrity-plugin";
+import usePluginsStore from "~/stores/plugins";
+
+export default defineNuxtPlugin({
+  name: "Hash Text",
+  parallel: false,
+  setup() {
+    const { addPlugin } = usePluginsStore();
+
+    addPlugin({
+      id: "hash-text",
+      type: AlacrityPluginType.hash,
+      icon: "fingerprint",
+    });
+  },
+});
